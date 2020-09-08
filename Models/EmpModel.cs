cb402e4a9ed82d25d55cc5e1bba32ed1723aeb28
Harshitha <harshitha.sivalenka@dxc.com>
2020-09-08 11:45:43 +0530

    First commit

---
+++ Models/EmpModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitExerciseMVC.Models
+{
+    public class EmpModel
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDoj { get; set; }
+    }
+}
